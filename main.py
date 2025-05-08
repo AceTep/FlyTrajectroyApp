561cb0b007454a65f668add636814e93100a993c
AceTep <72736849+AceTep@users.noreply.github.com>
2025-05-08 14:21:58 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# main.py
-import sys
-from PySide6.QtWidgets import QApplication
-from gui import MainWindow
-
-app = QApplication(sys.argv)
-window = MainWindow()
-window.show()
-sys.exit(app.exec())
